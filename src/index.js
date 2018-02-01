06756d0238e46a4f95f57f64650ffc170ef5c958
Timur <hasan13@me.com>
2018-02-01 16:56:50 +0300

    Removed useless lines from index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import AddClient from './AddClient';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<AddClient />, document.getElementById('root'));
+
